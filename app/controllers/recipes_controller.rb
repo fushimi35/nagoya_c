d0a6356213e728b61d1eeece10d29ac9cdfacaeb
Minako <Minako@Minako-no-MacBook-Air.local>
2022-07-09 15:48:03 +0900

    Add: レシピ投稿機能の追加

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,20 +3,35 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = current_user.recipes.new(recipe_params)
+    byebug
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :body)
   end
 end
