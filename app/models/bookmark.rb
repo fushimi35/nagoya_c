912df033c95d65fa44cf45af72309fcd0fd0b38b
大塚健登 <kent0tsuka1118@gmail.com>
2022-07-09 14:32:02 +0900

    Add：各モデルにリレーション設定を追加

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,6 @@
 class Bookmark < ApplicationRecord
+  belongs_to :user
+  belongs_to :recip
+
+  validates :user_id, uniqueness: { scope: :recip_id }
 end
--- app/models/recip.rb
+++ app/models/recip.rb
@@ -1,2 +1,4 @@
 class Recip < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
 end
